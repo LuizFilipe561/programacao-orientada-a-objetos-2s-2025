0662e1b15d922480af6edfcb7d915efb15fd30b7
LuizFilipe561 <luizoliveiragomes0@gmail.com>
2025-09-08 10:09:04 -0300

    salvo

---
+++ carro.java
@@ -0,0 +1,42 @@
+package sistema_radar;
+
+public class Carro {
+   private String placa;
+   private Integer velocidade;
+   private Integer ano;
+   private String modelo;
+
+   public Carro(String placa, Integer velocidade, Integer ano, String modelo){
+    this.placa = placa;
+    this.velocidade = velocidade;
+    this.ano = ano;
+    this.modelo = modelo;
+   }
+   
+   public Integer getVelocidade(){
+        return this.velocidade;
+   }
+
+   public void setVelocidade(Integer velocidade){
+    if(velocidade != null && velocidade > 0){
+        this.velocidade = velocidade;
+    }
+   }
+
+   public String getPlaca(){
+    return this.placa;
+   }
+
+   public void acelerar(){
+    this.velocidade += 10;
+   }
+
+   public void frear(){
+    if(this.velocidade > 0){
+        this.velocidade -= 10;
+    }
+   }
+   
+}
+    
+
